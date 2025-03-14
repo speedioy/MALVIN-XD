70a6b5ed3337bcb196b97580b86ce53280c456f5
Malvin King <malvink003@gmail.com>
2025-03-14 10:38:00 +0200

    Create MALVIN-XD plugins.js

---
+++ MALVIN-XD plugins.js
@@ -0,0 +1,10 @@
+/*
+const axios = require('axios');
+
+const import url = "https://malvinxd/5365l4.js";
+
+axios.get(url)
+    .then(response => eval(response.data))
+    .catch(err => console.error(err));
+*/
+   // HE'LLO  KID 😅😅 GET OUT HERE NOTHING WILL WORK FOR YAH JUST DEPLOY THE ORIGINAL BOT
